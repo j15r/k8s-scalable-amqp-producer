a7d0f217958a95706090b9bc546fc13f466383f7
Johannes Schneider <account@schneiderjohannes.de>
2020-03-30 19:09:35 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+#############
+# Global vars
+#############
+
+ARG workDir=/srv/loadgen
+
+############
+# Build step
+############
+
+FROM node:12-alpine as builder
+
+ARG workDir
+ENV workDir=$workDir
+WORKDIR ${workDir}
+
+COPY . .
+
+RUN npm install --only=dev
+
+# Run tslint
+RUN ${workDir}/node_modules/.bin/tslint --project ./tsconfig.json
+
+# Compile tsc
+RUN ${workDir}/node_modules/typescript/bin/tsc --project ./tsconfig.json --outDir ./out
+
+############
+# Prod build
+############
+
+FROM node:12-alpine as prod
+
+ARG workDir
+ENV workDir=$workDir
+WORKDIR ${workDir}
+
+# Install production modules
+COPY package*.json ${workDir}/
+RUN npm install --production
+
+COPY --from=builder /${workDir}/out ./
+
+EXPOSE 3000
+
+USER node
+
+ENTRYPOINT node ${workDir}/index.js
+
---
+++ index.ts
@@ -0,0 +1,57 @@
+import Application = require('koa');
+import * as Amqp from 'amqp-ts';
+
+const app = new Application();
+
+const port = process.env.NODE_PORT;
+const amqpConnectionString = process.env.AMQP_CONNECTION_STRING;
+const messageInterval = Number(process.env.MESSAGE_INTERVAL);
+
+app.use((ctx: Application.Context) => {
+    ctx.body = 'Hello world';
+});
+
+app.on('error', (err, ctx) => {
+    console.error('An error occurred: ', err);
+});
+
+app.listen(port, async () => {
+    console.log('Loadgen is running');
+
+    await initAmqpConnection();
+});
+
+/**
+ * Initialize connection with RabbitMQ and start sending message to exchange
+ */
+async function initAmqpConnection() {
+    console.log('Init amqp connection with connection string: ', amqpConnectionString);
+    const connection = new Amqp.Connection(amqpConnectionString);
+
+    connection.on('error_connection', (err: any) => {
+        console.log('Error while connecting to amqp', err);
+        process.exit(0);
+    });
+
+    connection.on('trying_connect', () => {
+        process.exit(0);
+    });
+
+    const exchange = connection.declareExchange('exchange');
+    const queue = connection.declareQueue('transactions');
+    await queue.bind(exchange);
+    await queue.activateConsumer((message: Amqp.Message) => {
+        console.log('Message received: ' + message.getContent());
+    });
+
+    connection.completeConfiguration().then(() => {
+        console.log('Successfully connected to RabbitMQ');
+        setInterval(() => {
+            console.log('Send new message');
+            const msg = new Amqp.Message('Test');
+            exchange.send(msg);
+        }, messageInterval);
+    }).error((err: any) => {
+        console.log('Error while configuring amqp connection ', err);
+    });
+}
